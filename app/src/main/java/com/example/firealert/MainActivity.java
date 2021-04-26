5d0cbfb8ad0aeb832da76f4f071fe00f008677dc
BinhPhamQuang <bnh.quang@gmail.com>
2021-04-26 22:11:24 +0700

    main acitivty

--- app/src/main/java/com/example/firealert/MainActivity.java
+++ app/src/main/java/com/example/firealert/MainActivity.java
@@ -1,14 +1,70 @@
 package com.example.firealert;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        progressBar= findViewById(R.id.progressBar1);
+        progressBar.setMax(30);
+        progressBar.getProgressDrawable().setColorFilter(
+                Color.rgb(50,130,184), android.graphics.PorterDuff.Mode.SRC_IN);
+        new AsyncLoading().execute();
+
+
+
+    }
+    private class AsyncLoading extends AsyncTask<Void,Integer,Void>
+    {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            for(int count=1;count<=30;count++)
+            {
+                try {
+                    Thread.sleep(30);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                publishProgress(count);
+            }
+            return null;
+
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            startActivity( new Intent(MainActivity.this,LoginActivity.class));
+            finish();
+
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            progressBar.setProgress(values[0]);
+
+        }
+
+
+
+
     }
 }
