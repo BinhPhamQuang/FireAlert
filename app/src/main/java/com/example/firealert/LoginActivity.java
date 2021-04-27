8a8ef539c3d36e4828b17f6389407e38e7ca3df2
BinhPhamQuang <bnh.quang@gmail.com>
2021-04-27 14:10:03 +0700

    home activity

--- app/src/main/java/com/example/firealert/LoginActivity.java
+++ app/src/main/java/com/example/firealert/LoginActivity.java
@@ -4,18 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     private TextView btnSignUp;
+
+    private ImageButton btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         btnSignUp= findViewById(R.id.btn_signUp);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,HomeActivity.class));
+            }
+        });
+
+
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
