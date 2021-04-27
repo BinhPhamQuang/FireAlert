744765dbde0d9acb25ffa34c1edfcbbaab972a09
BinhPhamQuang <bnh.quang@gmail.com>
2021-04-27 09:30:22 +0700

    sign up

--- app/src/main/java/com/example/firealert/LoginActivity.java
+++ app/src/main/java/com/example/firealert/LoginActivity.java
@@ -2,13 +2,26 @@ package com.example.firealert;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView btnSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btnSignUp= findViewById(R.id.btn_signUp);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/example/firealert/SignUpActivity.java
@@ -0,0 +1,19 @@
+package com.example.firealert;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+
+
+    }
+}
