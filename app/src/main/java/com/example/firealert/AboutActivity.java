1ebd7550ab237178f7e68ff915b65ee933599698
nvbien2000 <nvbien2000@gmail.com>
2021-05-03 14:16:27 +0700

    Add About screen

---
+++ app/src/main/java/com/example/firealert/AboutActivity.java
@@ -0,0 +1,24 @@
+package com.example.firealert;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        ImageButton btn_back = (ImageButton) findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+}
