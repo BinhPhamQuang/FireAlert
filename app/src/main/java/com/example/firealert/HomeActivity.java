cd6720c6b653a266eaae5a66ba2b73feda9fa471
nvbien2000 <nvbien2000@gmail.com>
2021-05-03 11:44:53 +0700

    Add Tips screen, but not complete. Should use ViewPager to switch between other Tips screens.

--- app/src/main/java/com/example/firealert/HomeActivity.java
+++ app/src/main/java/com/example/firealert/HomeActivity.java
@@ -2,7 +2,10 @@ package com.example.firealert;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,41 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ImageButton btnAddRoom = (ImageButton) findViewById(R.id.btnAddRoom);
+        btnAddRoom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent= new Intent(getApplicationContext(),AddRoomActivity.class);
+//                startActivity(intent);
+            }
+        });
+
+        ImageButton btnSettings = (ImageButton) findViewById(R.id.btnSettings);
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent= new Intent(getApplicationContext(),SettingsActivity.class);
+//                startActivity(intent);
+            }
+        });
+
+        ImageButton btnTips = (ImageButton) findViewById(R.id.btnTips);
+        btnTips.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(getApplicationContext(),TipsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton btnRoomDetail = (ImageButton) findViewById(R.id.btnRoomDetail);
+        btnRoomDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent= new Intent(getApplicationContext(),RoomDetailActivity.class);
+//                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/firealert/TipsActivity.java
@@ -0,0 +1,31 @@
+package com.example.firealert;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.ImageButton;
+
+public class TipsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tips);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        ImageButton btn_back = (ImageButton) findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+    }
+}
