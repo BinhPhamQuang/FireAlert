6d8194c18dedc25ff41553fe41b787d8f8da0cbe
BinhPhamQuang <bnh.quang@gmail.com>
2021-05-24 10:34:42 +0700

    create class user (data transfer object)

---
+++ app/src/main/java/com/example/firealert/DTO/User.java
@@ -0,0 +1,33 @@
+package com.example.firealert.DTO;
+
+public class User {
+
+    private  User()
+    {
+
+    }
+    public static User getInstance()
+    {
+        User user = new User();
+        return  user;
+    }
+    //set default value for test mode !
+    private int user_id=1;
+    private int house_id=1;
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public void setHouse_id(int house_id) {
+        this.house_id = house_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public int getHouse_id() {
+        return house_id;
+    }
+}
